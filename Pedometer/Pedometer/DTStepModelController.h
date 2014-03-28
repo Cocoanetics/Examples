e952938d0b11f2dd52cbe598df548986b37e2cb4
Oliver Drobnik <oliver@cocoanetics.com>
2014-03-28 12:50:32 +0100

    added CoreMotion

---
+++ Pedometer/Pedometer/DTStepModelController.h
@@ -0,0 +1,13 @@
+//
+//  DTStepModelController.h
+//  Pedometer
+//
+//  Created by Oliver Drobnik on 28.03.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTStepModelController : NSObject
+
+@end
---
+++ Pedometer/Pedometer/DTStepModelController.m
@@ -0,0 +1,16 @@
+//
+//  DTStepModelController.m
+//  Pedometer
+//
+//  Created by Oliver Drobnik on 28.03.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import "DTStepModelController.h"
+
+@implementation DTStepModelController
+{
+
+}
+
+@end
