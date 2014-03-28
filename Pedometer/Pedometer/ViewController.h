7128dab8a3a354a9caedf51e5247d5833327d758
Oliver Drobnik <oliver@cocoanetics.com>
2014-03-28 14:16:30 +0100

    implemented view controller

--- Pedometer/Pedometer/ViewController.h
+++ Pedometer/Pedometer/ViewController.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2014 Cocoanetics. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *stepsLabel;
 
 @end
--- Pedometer/Pedometer/ViewController.m
+++ Pedometer/Pedometer/ViewController.m
@@ -7,23 +7,61 @@
 //
 
 #import "ViewController.h"
+#import "DTStepModelController.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+{
+   DTStepModelController *_stepModel;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+   
+   _stepModel = [[DTStepModelController alloc] init];
+   
+   [_stepModel addObserver:self forKeyPath:@"stepsToday" options:NSKeyValueObservingOptionNew context:NULL];
+   
+   [self _updateSteps:_stepModel.stepsToday];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)dealloc
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   [_stepModel removeObserver:self forKeyPath:@"stepsToday"];
+}
+
+- (void)_updateSteps:(NSInteger)steps
+{
+   // force main queue for UIKit
+   dispatch_async(dispatch_get_main_queue(), ^{
+      if (steps>=0)
+      {
+         self.stepsLabel.text = [NSString stringWithFormat:@"%ld",
+                                 (long)steps];
+         self.stepsLabel.textColor = [UIColor colorWithRed:0
+                                                     green:0.8
+                                                      blue:0
+                                                     alpha:1];
+      }
+      else
+      {
+         self.stepsLabel.text = @"Not available";
+         self.stepsLabel.textColor = [UIColor redColor];
+      }
+   });
+}
+
+#pragma mark - Notifications
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context
+{
+   [self _updateSteps:_stepModel.stepsToday];
 }
 
 @end
